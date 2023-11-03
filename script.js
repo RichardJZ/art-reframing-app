f6c0bf6d4429635976430a19891653bc3c1f6f81
RichardJZ <richardjucewicz@gmail.com>
2023-11-03 22:13:34 +0100

    Add files via upload
    praktisch alles erin

--- script.js
+++ script.js
@@ -1,77 +1,74 @@
-// JavaScript variables
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
-const clearButton = document.getElementById("clear-button");
-const imageUpload = document.getElementById("image-upload");
+document.addEventListener("DOMContentLoaded", function() {
+    const imageUpload = document.getElementById("image-upload");
+    const brightnessSlider = document.getElementById("brightness-slider");
+//    const widthSlider = document.getElementById("width-slider");
+//     const heightSlider = document.getElementById("height-slider");
+    const uploadedImage = document.getElementById("uploaded-image");
+    const backgroundColorSlider = document.getElementById("background-color-slider");
 
-let isDrawing = false;
-let lastX = 0;
-let lastY = 0;
+    imageUpload.addEventListener("change", handleImageUpload);
+    brightnessSlider.addEventListener("input", updateBrightness);
+  //  widthSlider.addEventListener("input", updateDimensions);
+  //  heightSlider.addEventListener("input", updateDimensions);
+    backgroundColorSlider.addEventListener("input", updateBackgroundColor);
 
-// Set up canvas
-canvas.width = window.innerWidth - 20;
-canvas.height = 400;
-context.lineWidth = 5;
-context.lineJoin = "round";
-context.lineCap = "round";
-context.strokeStyle = "#000";
+    function handleImageUpload(e) {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
 
-// Event listeners
-canvas.addEventListener("mousedown", (e) => {
-    isDrawing = true;
-    [lastX, lastY] = [e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top];
-});
-
-canvas.addEventListener("mousemove", draw);
-
-canvas.addEventListener("mouseup", () => isDrawing = false);
-canvas.addEventListener("mouseout", () => isDrawing = false);
-
-clearButton.addEventListener("click", clearCanvas);
-
-imageUpload.addEventListener("change", handleImageUpload);
-
-// Drawing function
-function draw(e) {
-    if (!isDrawing) return;
-
-    const [x, y] = [e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top];
-
-    context.beginPath();
-    context.moveTo(lastX, lastY);
-    context.lineTo(x, y);
-    context.stroke();
-
-    [lastX, lastY] = [x, y];
-}
-
-// Clear the canvas
-function clearCanvas() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-// Handle image upload
-// Handle image upload
-function handleImageUpload(e) {
-    const file = e.target.files[0];
-    if (file) {
-        const reader = new FileReader();
-
-        reader.onload = function (event) {
-            const img = new Image();
-            img.src = event.target.result;
-
-            img.onload = function () {
-                // Set canvas dimensions to match the image
-                canvas.width = img.width;
-                canvas.height = img.height;
-
-                // Draw the uploaded image on the canvas
-                context.clearRect(0, 0, canvas.width, canvas.height);
-                context.drawImage(img, 0, 0, canvas.width, canvas.height);
+            reader.onload = function(event) {
+                uploadedImage.src = event.target.result;
+                // Reset sliders to default values
+                brightnessSlider.value = 100;
+            //    widthSlider.value = 100;
+            //    heightSlider.value = 100;
             };
-        };
 
-        reader.readAsDataURL(file);
+            reader.readAsDataURL(file);
+        }
     }
-}
+
+    function updateBrightness() {
+        const value = brightnessSlider.value;
+        uploadedImage.style.filter = `brightness(${value}%)`;
+    }
+/*
+    function updateDimensions() {
+        const widthValue = widthSlider.value;
+        const heightValue = heightSlider.value;
+        uploadedImage.style.width = `${widthValue}%`;
+        uploadedImage.style.height = `${heightValue}%`;
+    } */
+
+    function updateBackgroundColor() {
+        const colorValue = backgroundColorSlider.value;
+        document.body.style.backgroundColor = `rgb(${colorValue}, ${colorValue}, ${colorValue})`;
+    }
+  
+
+
+    const points = document.querySelectorAll('.point');
+    const bText = document.getElementById('bottomText');
+    const lText = document.getElementById('leftText');
+    const tText = document.getElementById('topText');
+    const rText = document.getElementById('rightText');
+
+    const words = ['honger', 'dierlijkheid', 'woede','kalmte',
+    'hemel', 'leren', 'realisatie', 'bodhisattva', 'boeddha'];
+
+    points.forEach((point, index) => {
+        point.addEventListener('click', () => {
+            bText.textContent = words[index];
+            bText.style.display = 'block';
+            lText.textContent = words[index];
+            lText.style.display = 'block';
+            tText.textContent = words[index];
+            tText.style.display = 'block';
+            rText.textContent = words[index];
+            rText.style.display = 'block';
+        });
+    });
+
+
+});
